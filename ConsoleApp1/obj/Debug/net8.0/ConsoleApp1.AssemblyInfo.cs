55b98e71ab5e68d086792b65e5e8edc75b3cbd29
Anisatuzzumara <anisatuzzumara13@gmail.com>
2025-05-15 10:38:49 +0700

    init add NilaiMahasiswa

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aec3de9bd403cc6c6b24ae7d13fd47ba40a681df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3db901ff891e55e7280e32f8946ef7eef076205d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ NilaiMahasiswa/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace Mahasiswa
+{
+    public class NilaiMhs
+    {
+        public string Nama { get; set; }
+        public int Nilai { get; set; }
+
+        public NilaiMhs(string nama, int nilai)
+        {
+            Nama = nama;
+            Nilai = nilai;
+        }
+
+        public string NilaiNya()
+        {
+            if (Nilai >= 85) return "A";
+            else if (Nilai >= 75) return "B";
+            else if (Nilai >= 65) return "C";
+            else return "D";
+        }
+
+
+        public void TampilkanHasil()
+        {
+            Console.WriteLine($"Hallo Nama: {Nama} , Kamu mendapatkan nilai: {Nilai} , Dengan Grade: {NilaiNya()}");
+
+        }
+    }
+    class Hasil
+    {
+        static void Main(string[] args)
+        {
+            NilaiMhs nilaimhs1 = new NilaiMhs("Sherina", 89);
+            NilaiMhs nilaimhs2 = new NilaiMhs("Agung", 76);
+            NilaiMhs nilaimhs3 = new NilaiMhs("Anindita", 70);
+            NilaiMhs nilaimhs4 = new NilaiMhs("Anugerah", 60);
+
+            nilaimhs1.TampilkanHasil();
+            nilaimhs2.TampilkanHasil();
+            nilaimhs3.TampilkanHasil();
+            nilaimhs4.TampilkanHasil();
+        }
+
+                
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
