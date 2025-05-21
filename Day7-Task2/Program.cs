147f3d0aebea6e6288b066c8c1d97151c845bc37
Anisatuzzumara <anisatuzzumara13@gmail.com>
2025-05-21 11:43:53 +0700

    Add files via upload

---
+++ Day7-Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Task
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 105;
+
+            for (int i = 1; i <= n; i++)
+            {
+                string hasil = "";
+
+                if (i % 3 == 0)
+                hasil += "foo";
+
+                if (i % 5 == 0)
+                hasil += "bar";
+
+                if (i % 7 == 0)
+                hasil += "jazz";
+
+                if (hasil == "")
+                hasil = i.ToString();
+                
+                Console.Write(hasil);
+
+                if (i != n)
+                    Console.Write(", ");
+        }
+            Console.WriteLine();
+    }
+}
+
+}
+
+
